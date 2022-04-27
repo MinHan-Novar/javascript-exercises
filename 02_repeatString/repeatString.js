adc088af3324a59e64b0128cacc55ad2d9c0516a
MinHan-Novar <minhankyaw.mh@gmail.com>
2022-04-27 22:09:25 +0630

    finished all exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,count) {
+    if (count < 0)return 'ERROR'
+    let string = '';
+    for(let i =0;l = count,i<l;i++){
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
